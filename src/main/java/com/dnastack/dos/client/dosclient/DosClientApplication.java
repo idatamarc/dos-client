d43e2c4f18f5274a2036cd73c0a972cfc5942f8d
Marc Huang <marc.huang@scotiabank.com>
2018-03-18 17:31:01 -0400

    project scaffolding

---
+++ src/main/java/com/dnastack/dos/client/dosclient/DosClientApplication.java
@@ -0,0 +1,12 @@
+package com.dnastack.dos.client.dosclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosClientApplication.class, args);
+	}
+}
